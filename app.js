f63830a090c2d923b2d352ba2982e25be8ea8e27
wilburlua910 <wilburlua910@outlook.com>
2023-03-30 02:05:33 +0800

    BE change

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var sendVideo = require('./routes/sendVideo');
 
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var privateKey  = fs.readFileSync('../selfsigned.key', 'utf8');
+var certificate = fs.readFileSync('../selfsigned.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
 var app = express();
